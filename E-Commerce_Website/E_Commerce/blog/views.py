2e8ffc268733b60c2602b74f3316f4d1a9783736
EshwarNaikG <eshwarnaik79@gmail.com>
2026-01-01 19:17:42 +0530

    shop apps creates

---
+++ E-Commerce_Website/E_Commerce/blog/views.py
@@ -0,0 +1,8 @@
+def index(request):
+    return HttpResponse("Index shop")# from django.shortcuts import render
+from  django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Index blog")
